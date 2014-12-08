170514d30710f55828d272fb72361b848400ca25
unknown <gregoire.piat@icof.fr>
2014-12-08 11:00:20 +0100

    Modification du nom du form par gregoire

---
+++ ppeGsbCSharp/FormGsbBO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ppeGsbCSharp
+{
+    public partial class FormGsb : Form
+    {
+        public FormGsb()
+        {
+            InitializeComponent();
+        }
+
+        private void lblVisiteurRdv_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabClients_Click(object sender, EventArgs e)
+        {
+            if (cbxNomClient.Text == null)
+            {
+                dgvAgendaClient.Enabled = false;
+                gpbAgendaClient.Enabled = false;
+                rtbRdvClient.Enabled = false;
+                dateRdvClient.Enabled = false;
+                cbxVisiteurAjoutRdvClient.Enabled = false;
+            }
+        }
+    }
+}
