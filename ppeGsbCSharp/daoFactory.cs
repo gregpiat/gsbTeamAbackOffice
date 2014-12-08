2e343e912afab7bd3534de911de3373de42eac05
unknown <gregoire.piat@icof.fr>
2014-12-08 10:46:37 +0100

    Ajout de la classe daoClient et daoFactory au projet par gregoire

--- ppeGsbCSharp/daoFactory.cs
+++ ppeGsbCSharp/daoFactory.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
 
 namespace ppeGsbCSharp
 {
     class daoFactory
     {
-
+        private void Initialize()
+        {
+            SqlConnection myConnection = new SqlConnection("user id=piat;" +
+                                       "password=btssio;server=172.17.21.10;" +
+                                       "Trusted_Connection=yes;" +
+                                       "database=gbsTeamA; " +
+                                       "connection timeout=15");
+        }
     }
 }
