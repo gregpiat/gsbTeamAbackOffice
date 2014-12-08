7605b231f67750d6cde18f725dcbc6075578868a
unknown <gregoire.piat@icof.fr>
2014-12-08 11:56:56 +0100

    Modification du daoFactory par gregoire

--- ppeGsbCSharp/daoClient.cs
+++ ppeGsbCSharp/daoClient.cs
@@ -2,20 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections;
 
 
 namespace ppeGsbCSharp
 {
     class daoClient
     {
-        public static void modifierClient(String unNom, String unCode, String uneRaison, String uneAdresse, String unType, String unTel, String unMail, DateTime uneDate){
-            daoFactory.Initialize();
+        public List<Client> tousLesClients;
 
-            
-        }
-        public static Client recupererLesClients()
+        public static List<Client> recupererLesClients()
         {
+            String requete = "SELECT * FROM client;";
 
+            tousLesClients = new List<Client>();
+            return tousLesClients;
+        }
+        public static void modifierClient(int unId, String unNom, String unCode, String uneRaison, String uneAdresse, String unType, String unTel, String unMail, DateTime uneDate){
+            daoFactory.connexionBDD();
+            String requete = "UPDATE client SET (nom =" + unNom + 
+                                                ", code =" + unCode + 
+                                                ", raison=" + uneRaison + 
+                                                "adresse=" + uneAdresse + 
+                                                ", type =" + unType + 
+                                                ", tel =" + unTel + 
+                                                ", mail =" + unMail + 
+                                                ",date =" + uneDate+
+                                                ") where id ="+unId+";";
+        }
+
+        public static void supprimerClient(int unId)
+        {
+            String requete = "DELETE FROM client WHERE id =" + unId + ";";
         }
     }
 }
--- ppeGsbCSharp/daoFactory.cs
+++ ppeGsbCSharp/daoFactory.cs
@@ -4,17 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Data.SqlClient;
 using System.Data.Sql;
+using System.Windows.Forms;
 
 namespace ppeGsbCSharp
 {
     class daoFactory
     {
-            SqlConnection myConnection = new SqlConnection("user id=piat;" +
-                                       "password=btssio;server=172.17.21.10;" +
+        SqlConnection connexionBDD;
+
+        public daoFactory()
+        {
+            Initialize();
+        }
+
+        public void Initialize(){
+            connexionBDD = new SqlConnection("user id=piat;" +
+                                       "password=btssio-2015;server=172.17.21.10;" +
                                        "Trusted_Connection=yes;" +
                                        "database=gbsTeamA; " +
                                        "connection timeout=15");
-        public static connexionBDD(){
+        }
+
+        public bool ouvrirConnexion(){
+            try{
+                connexionBDD.Open();
+                return true;
+             }
+            catch (SqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Impossible de se connecter au serveur. Contacter l'administrateur");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("utilisateur/mdp invalide");
+                        break;
+                }
+                return false;
+            }
 
         }
     }
