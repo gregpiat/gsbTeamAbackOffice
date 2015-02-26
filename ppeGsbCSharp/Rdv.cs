cc9bdb66c7d3700acb76b86f3b15852a2de2ae2a
unknown <gregoire.piat@icof.fr>
2015-02-26 14:21:20 +0100

    Ajout du Rdv.cs par Grégoire

---
+++ ppeGsbCSharp/Rdv.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ppeGsbCSharp
+{
+    class Rdv
+    {
+        int id;
+        Client client;
+        DateTime date;
+
+        public Rdv(int unId, Client unClient, DateTime uneDate){
+            id = unId;
+            client = unClient;
+            date = uneDate;
+        }
+    }
+}
